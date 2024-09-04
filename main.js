19ed61d465216b9aa6de2e3f0efac255132bc82a
esraarabee1 <esraarabee69@gmail.com>
2024-09-04 11:31:11 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var selectorField=document.getElementById("selectorField");
+var selectText=document.getElementById("selectText");
+var options=document.getElementsByClassName("options");
+var list= document.getElementById("list");
+var arrowIcon=document.getElementById("arrowIcon");
+
+selectorField.onclick=function(){
+list.classList.toggle("hide");
+arrowIcon.classList.toggle("rotate");
+
+}
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML=this.textContent;
+        list.classList.toggle("hide");
+        arrowIcon.classList.toggle("rotate");
+
+    }
+}
